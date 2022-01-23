9702dcd9efd34096691de9f1ba85d13c35c66b8b
Gaurav Appa <gauravappagga@gmail.com>
2022-01-23 17:08:39 +0530

    first commit

---
+++ src/test/java/com/java/oop/OopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
